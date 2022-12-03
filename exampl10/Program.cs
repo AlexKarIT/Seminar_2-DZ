760cfcd8b93167471c0a567078d2599a8f59abca
Alex <akaravashkin5@gmail.com>
2022-12-03 16:04:58 +0300

    DZ

---
+++ exampl10/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = new Random().Next(100, 1000); 
+Console.WriteLine("Сгенерированное число: " + number);
+
+int secondDigit = number/10;
+secondDigit = secondDigit % 10;
+
+// int max = firstDigit;
+// if (secondDigit > max)
+// {
+//     max = secondDigit;
+// }
+
+Console.WriteLine("Вторая цифра: " + secondDigit);
---
+++ exampl13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int number = new Random().Next(1, 10000000);
+Console.WriteLine("Сгенерированное число:" + number);
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while ((number / 100) >= 10)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = number%10;
+    Console.WriteLine("Третья цифра: " + thirdDigit);
+}
